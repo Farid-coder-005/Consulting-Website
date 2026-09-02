e20034da09424d66cd06580c2a2e7e2c64f7afb9
Ferid <zkryyevfrd1912@gmail.com>
2026-09-03 02:01:44 +0400

    style: replace header logo image source with logo2

--- frontend/src/content.ts
+++ frontend/src/content.ts
@@ -1,4 +1,4 @@
-export const LOGO_URL = "/logo.png";
+export const LOGO_URL = "/logo2.png";
 
 export const HERO_BG =
   "https://gurcanpartners.com/wp-content/uploads/gurcan-partners-faaliyet-alanlarimiz.jpg";
